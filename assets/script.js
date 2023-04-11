8cb4b47824929f2702ce773f2f61df7bbf49d36f
Edward Kim <edwardkim@Edwards-MacBook-Pro.local>
2023-04-11 12:34:38 -0400

    added functions to JavaScript

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,32 @@
 var topics = ["HTML", "CSS", "Git", "JavaScript"];
+var randomNum = Math.floor(Math.random() * topics.length);
+var randomTopic = topics[randomNum];
 
-for (let i = 0; i < topics.length; i++) {
-    console.log(topics[i])
+function listTopics() {
+    for (let i = 0; i < topics.length; i++) {
+        console.log(topics[i])
+    }
 }
 
-/*
-
-var topic = "HTML";
-
-if (topic === 'HTML') {
-    console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-    console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-    console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-    console.log("Let's study JavaScript!");
-} else {
-    console.log("Please try again!")
+function selectTopic() {
+    if (randomTopic === 'HTML') {
+        console.log("Let's study HTML!");
+    } else if (randomTopic === 'CSS') {
+        console.log("Let's study CSS!");
+    } else if (randomTopic === 'Git') {
+        console.log("Let's study Git!");
+    } else if (randomTopic === 'JavaScript') {
+        console.log("Let's study JavaScript!");
+    } else {
+        console.log("Please try again!");
+    }
 }
 
-*/
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+
+console.log('Which topic should we study first?');
+selectTopic();
+
+
 
